e82dc70d8b6e21ea7a04d5a9a52b672728c297c7
Florian Bruder <florian.bruder@posteo.de>
2022-02-12 09:15:25 +0100

    Implement Reminder type + tests

--- TodayTests/ReminderTests.swift
+++ TodayTests/ReminderTests.swift
@@ -5,8 +5,32 @@
 //  Created by Florian Bruder on 12.02.22.
 //
 
+@testable import Today
 import XCTest
 
 // MARK: -
 
 final class ReminderTestCase: XCTestCase {}
+
+// MARK: -
+
+extension ReminderTestCase {
+    func testAllProperties() {
+        let title = "TestTitle"
+        let dueDate = Date()
+        let notes = "Test notes"
+        let isComplete = true
+
+        let reminder = Reminder(
+            title: title,
+            dueDate: dueDate,
+            notes: notes,
+            isComplete: isComplete)
+
+        XCTAssertEqual(title, reminder.title)
+        XCTAssertEqual(dueDate, reminder.dueDate)
+        XCTAssertNotNil(reminder.notes)
+        XCTAssertEqual(notes, reminder.notes)
+        XCTAssertEqual(isComplete, reminder.isComplete)
+    }
+}
