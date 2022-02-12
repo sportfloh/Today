717b8462b1b442abb0ee8a60646a72b4cd983c04
Florian Bruder <florian.bruder@posteo.de>
2022-02-12 08:46:11 +0100

    Add ReminderTests.swift

--- TodayTests/ReminderTests.swift
+++ TodayTests/ReminderTests.swift
@@ -1,13 +1,12 @@
 //
-//  TodayTests.swift
+//  ReminderTests.swift
 //  TodayTests
 //
 //  Created by Florian Bruder on 12.02.22.
 //
 
-@testable import Today
 import XCTest
 
 // MARK: -
 
-final class TodayTests: XCTestCase {}
+final class ReminderTestCase: XCTestCase {}
