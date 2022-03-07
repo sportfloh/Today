27be82f635d7ecdf4c525b887e4692bebf1f33e3
Florian Bruder <florian.bruder@posteo.de>
2022-03-07 17:13:53 +0100

    Add Date+Today.swift and Date+TodayTests.swift

---
+++ Today/Models/Date+Today.swift
@@ -0,0 +1,10 @@
+//
+//  Date+Today.swift
+//  Today
+//
+//  Created by Florian Bruder on 07.03.22.
+//
+
+import Foundation
+
+// MARK: -
---
+++ TodayTests/Date+TodayTests.swift
@@ -0,0 +1,12 @@
+//
+//  Date+TodayTests.swift
+//  TodayTests
+//
+//  Created by Florian Bruder on 07.03.22.
+//
+
+import XCTest
+
+// MARK: -
+
+final class Date_TodayTestCase: XCTestCase {}
