158c0236cee2e6026c553aedb6ef96fa907db26e
Florian Bruder <florian.bruder@posteo.de>
2022-03-07 18:36:34 +0100

    Add dayAndTimeText + tests

--- Today/Models/Date+Today.swift
+++ Today/Models/Date+Today.swift
@@ -8,3 +8,21 @@
 import Foundation
 
 // MARK: -
+
+extension Date {
+    var dayAndTimeText: String {
+        let timeText = formatted(date: .omitted, time: .shortened)
+        if Locale.current.calendar.isDateInToday(self) {
+            let timeFormat = NSLocalizedString(
+                "Today at %@",
+                comment: "Today at time format string")
+            return String(format: timeFormat, timeText)
+        } else {
+            let dateText = formatted(.dateTime.month(.abbreviated).day())
+            let dateAndTimeFormat = NSLocalizedString(
+                "%@ at %@",
+                comment: "Date and time format string")
+            return String(format: dateAndTimeFormat, dateText, timeText)
+        }
+    }
+}
--- TodayTests/Date+TodayTests.swift
+++ TodayTests/Date+TodayTests.swift
@@ -5,8 +5,43 @@
 //  Created by Florian Bruder on 07.03.22.
 //
 
+import Foundation
+@testable import Today
 import XCTest
 
 // MARK: -
 
-final class Date_TodayTestCase: XCTestCase {}
+// swiftlint:disable type_name
+final class Date_TodayTestCase: XCTestCase {
+    // MARK: - test property dayAndTime
+
+    func testDayAndTimeTextToday() {
+        let timeText = Date().formatted(date: .omitted, time: .shortened)
+        let timeFormat = NSLocalizedString(
+            "Today at %@",
+            comment: "Today at time format string")
+
+        XCTAssertEqual(
+            String(format: timeFormat, timeText),
+            Date().dayAndTimeText)
+    }
+
+    func testDayAndTimeTextNotToday() {
+        let seconds: TimeInterval = 2000
+        let dateText = Date(timeIntervalSince1970: seconds)
+            .formatted(.dateTime.month(.abbreviated).day())
+        let timeText = Date(timeIntervalSince1970: seconds)
+            .formatted(date: .omitted, time: .shortened)
+        let dateAndTimeFormat = NSLocalizedString(
+            "%@ at %@",
+            comment: "Date and time format string")
+
+        XCTAssertEqual(
+            String(
+                format: dateAndTimeFormat,
+                dateText, timeText),
+            Date(timeIntervalSince1970: seconds).dayAndTimeText)
+    }
+
+    // MARK: - test property dayAndTime
+}
