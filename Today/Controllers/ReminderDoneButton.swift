c322fc9f6be933209e2911ebf96bacf9888e8513
Florian Bruder <florian.bruder@posteo.de>
2022-03-14 20:11:29 +0100

    Add ReminderDoneButton.swift

---
+++ Today/Controllers/ReminderDoneButton.swift
@@ -0,0 +1,14 @@
+//
+//  ReminderDoneButton.swift
+//  Today
+//
+//  Created by Florian Bruder on 14.03.22.
+//
+
+import UIKit
+
+// MARK: -
+
+class ReminderDoneButton: UIButton {
+    var id: Reminder.ID?
+}
