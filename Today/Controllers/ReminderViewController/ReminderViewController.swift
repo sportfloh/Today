547ce18cf2b11100b3456eb27f44d4091c474d2b
Florian Bruder <florian.bruder@posteo.de>
2022-03-21 14:54:40 +0100

    Add cell registration handler

--- Today/Controllers/ReminderViewController/ReminderViewController.swift
+++ Today/Controllers/ReminderViewController/ReminderViewController.swift
@@ -30,7 +30,30 @@ class ReminderViewController: UICollectionViewController {
         fatalError("Always initialize ReminderViewController using init(reminder:)")
     }
 
-    func test(for row: Row) -> String? {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
+        dataSource = DataSource(
+            collectionView: collectionView
+        ) { (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: Row) in
+            collectionView.dequeueConfiguredReusableCell(
+                using: cellRegistration,
+                for: indexPath,
+                item: itemIdentifier
+            )
+        }
+    }
+
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, row: Row) {
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = text(for: row)
+        contentConfiguration.textProperties.font = UIFont.preferredFont(forTextStyle: row.textStyle)
+        contentConfiguration.image = row.image
+        cell.contentConfiguration = contentConfiguration
+        cell.tintColor = .todayPrimaryTint
+    }
+
+    func text(for row: Row) -> String? {
         switch row {
         case .viewDate:
             return reminder.dueDate.dayText
