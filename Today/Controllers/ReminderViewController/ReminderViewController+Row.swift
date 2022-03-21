f2b981a680d25a01ab1a305906e54257e251caa8
Florian Bruder <florian.bruder@posteo.de>
2022-03-21 14:03:54 +0100

    Add Row enum

--- Today/Controllers/ReminderViewController/ReminderViewController+Row.swift
+++ Today/Controllers/ReminderViewController/ReminderViewController+Row.swift
@@ -6,5 +6,42 @@
 //
 
 import Foundation
+import UIKit
 
 // MARK: -
+
+extension ReminderListViewController {
+    enum Row: Hashable {
+        case viewDate
+        case viewNotes
+        case viewTime
+        case viewTitle
+
+        var imageName: String? {
+            switch self {
+            case .viewDate: return "calendar.circle"
+            case .viewNotes: return "square.and.pencil"
+            case .viewTime: return "clock"
+            default: return nil
+            }
+        }
+
+        var image: UIImage? {
+            guard let imageName = imageName else {
+                return nil
+            }
+
+            let configuration = UIImage.SymbolConfiguration(textStyle: .headline)
+            return UIImage(systemName: imageName, withConfiguration: configuration)
+        }
+
+        var textStyle: UIFont.TextStyle {
+            switch self {
+            case .viewTitle:
+                return .headline
+            default:
+                return .subheadline
+            }
+        }
+    }
+}
