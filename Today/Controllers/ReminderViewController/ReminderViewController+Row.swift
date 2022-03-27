c406db129c81c0e8bf4f2c87c71cf0032427a643
Florian Bruder <florian.bruder@posteo.de>
2022-03-27 23:20:13 +0200

    Update cell configuration to support date and notes

--- Today/Controllers/ReminderViewController/ReminderViewController+Row.swift
+++ Today/Controllers/ReminderViewController/ReminderViewController+Row.swift
@@ -17,7 +17,8 @@ extension ReminderViewController {
         case viewNotes
         case viewTime
         case viewTitle
-        case editText(String)
+        case editDate(Date)
+        case editText(String?)
 
         var imageName: String? {
             switch self {
