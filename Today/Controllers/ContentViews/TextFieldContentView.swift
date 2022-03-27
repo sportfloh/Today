8310c8024ffaee9dcb5198f0fe499f0f57d711fc
Florian Bruder <florian.bruder@posteo.de>
2022-03-27 20:54:35 +0200

    Add custom TextFieldContentView

--- Today/Controllers/ContentViews/TextFieldContentView.swift
+++ Today/Controllers/ContentViews/TextFieldContentView.swift
@@ -6,5 +6,25 @@
 //
 
 import Foundation
+import UIKit
 
 // MARK: -
+
+class TextFieldContentView: UIView {
+    let textField = UITextField()
+
+    override var intrinsicContentSize: CGSize {
+        CGSize(width: 0, height: 44)
+    }
+
+    init() {
+        super.init(frame: .zero)
+        addPinnedSubview(textField, insets: UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16))
+        textField.clearButtonMode = .whileEditing
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
