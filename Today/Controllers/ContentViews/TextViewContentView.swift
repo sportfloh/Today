4e3b23f0f89dda44c9481a45e19794b6050c3a0f
Florian Bruder <florian.bruder@posteo.de>
2022-03-27 22:42:35 +0200

    Add TextViewContentView

--- Today/Controllers/ContentViews/TextViewContentView.swift
+++ Today/Controllers/ContentViews/TextViewContentView.swift
@@ -8,3 +8,52 @@
 import UIKit
 
 // MARK: -
+
+class TextViewContentView: UIView, UIContentView {
+    struct Configuration: UIContentConfiguration {
+        var text: String? = ""
+
+        func makeContentView() -> UIView & UIContentView {
+            TextViewContentView(self)
+        }
+    }
+
+    let textView = UITextView()
+    var configuration: UIContentConfiguration {
+        didSet {
+            configure(configuration: configuration)
+        }
+    }
+
+    override var intrinsicContentSize: CGSize {
+        CGSize(width: 0, height: 44)
+    }
+
+    init(_ configuration: UIContentConfiguration) {
+        self.configuration = configuration
+        super.init(frame: .zero)
+        addPinnedSubview(textView, hight: 200)
+        textView.backgroundColor = nil
+        textView.font = UIFont.preferredFont(forTextStyle: .body)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(configuration: UIContentConfiguration) {
+        guard let configuration = configuration as? Configuration else {
+            return
+        }
+        textView.text = configuration.text
+    }
+}
+
+// MARK: -
+
+extension TextViewContentView {
+    func textViewConfiguration() -> TextViewContentView.Configuration {
+        TextViewContentView.Configuration()
+    }
+}
