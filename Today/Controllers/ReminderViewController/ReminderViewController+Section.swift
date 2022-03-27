ac62695d4cb2f97a657b4f10147015afcf96bd5c
Florian Bruder <florian.bruder@posteo.de>
2022-03-27 14:45:46 +0200

    Use section enum for DiffableDatasource and DiffableDataSourceSnapshot

--- Today/Controllers/ReminderViewController/ReminderViewController+Section.swift
+++ Today/Controllers/ReminderViewController/ReminderViewController+Section.swift
@@ -8,3 +8,25 @@
 import Foundation
 
 // MARK: -
+
+extension ReminderViewController {
+    enum Section: Int, Hashable {
+        case view
+        case title
+        case date
+        case notes
+
+        var name: String {
+            switch self {
+            case .view:
+                return ""
+            case .title:
+                return NSLocalizedString("Title", comment: "Title section name")
+            case .date:
+                return NSLocalizedString("Date", comment: "Date section name")
+            case .notes:
+                return NSLocalizedString("Notes", comment: "Notes section name")
+            }
+        }
+    }
+}
