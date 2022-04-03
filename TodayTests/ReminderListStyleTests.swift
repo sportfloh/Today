545640345cf8b2af01280048fede33f4081dbacf
Florian Bruder <florian.bruder@posteo.de>
2022-04-03 19:23:17 +0200

    Add ReminderListStyleTests.swift

---
+++ TodayTests/ReminderListStyleTests.swift
@@ -0,0 +1,13 @@
+//
+//  ReminderListStyleTests.swift
+//  TodayTests
+//
+//  Created by Florian Bruder on 03.04.22.
+//
+
+@testable import Today
+import XCTest
+
+// MARK: -
+
+final class ReminderListStyleTestCase: XCTestCase {}
