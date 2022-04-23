d935ccbd583f4de7bfe1981466b9d77c9e0d1ab8
Florian Bruder <florian.bruder@posteo.de>
2022-04-23 17:27:14 +0200

    Set the progress

--- Today/Controllers/ListViewController/ReminderListViewController+DataSource.swift
+++ Today/Controllers/ListViewController/ReminderListViewController+DataSource.swift
@@ -31,6 +31,7 @@ extension ReminderListViewController {
             snapshot.reloadItems(ids)
         }
         dataSource.apply(snapshot)
+        headerView?.progress = progress
     }
 
     func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: Reminder.ID) {
