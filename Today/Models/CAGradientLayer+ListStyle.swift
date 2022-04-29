9362059994d28a80018fe58ac76b7dec8ef65759
Florian Bruder <florian.bruder@posteo.de>
2022-04-29 22:17:09 +0200

    Add extension for CAGradientLayer

--- Today/Models/CAGradientLayer+ListStyle.swift
+++ Today/Models/CAGradientLayer+ListStyle.swift
@@ -8,3 +8,31 @@
 import UIKit
 
 // MARK: -
+
+extension CAGradientLayer {
+    static func gradiantLayer(for style: ReminderListStyle, in frame: CGRect) -> Self {
+        let layer = Self()
+        layer.colors = colors(for: style)
+        layer.frame = frame
+        return layer
+    }
+
+    private static func colors(for style: ReminderListStyle) -> [CGColor] {
+        let beginColor: UIColor
+        let endColor: UIColor
+
+        switch style {
+        case .today:
+            beginColor = .todayGradientAllBegin
+            endColor = .todayGradientAllEnd
+        case .future:
+            beginColor = .todayGradientFutureBegin
+            endColor = .todayGradientFutureEnd
+        case .all:
+            beginColor = .todayGradientTodayBegin
+            endColor = .todayGradientTodayEnd
+        }
+
+        return [beginColor.cgColor, endColor.cgColor]
+    }
+}
