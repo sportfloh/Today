4a0708e654bd5feab7f48288c647faa71be67346
Florian Bruder <florian.bruder@posteo.de>
2022-05-01 17:30:55 +0200

    Add an error if calendar data access is not allowed

--- Today/Models/TodayError.swift
+++ Today/Models/TodayError.swift
@@ -10,11 +10,14 @@ import Foundation
 // MARK: -
 
 enum TodayError: LocalizedError {
+    case accessDenied
     case failedReadingReminders
     case reminderHasNoDueDate
 
     var errorDescription: String? {
         switch self {
+        case .accessDenied:
+            return NSLocalizedString("The app doesn't have permission to read reminders.", comment: "access denied error description")
         case .failedReadingReminders:
             return NSLocalizedString("Failed to read reminders.", comment: "failed reading reminders error description")
         case .reminderHasNoDueDate:
