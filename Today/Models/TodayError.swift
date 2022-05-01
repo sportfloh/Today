c9a069edccc789bc5b1c355cf96e1e30e4daa868
Florian Bruder <florian.bruder@posteo.de>
2022-05-01 16:46:23 +0200

    Add TodayError type

--- Today/Models/TodayError.swift
+++ Today/Models/TodayError.swift
@@ -8,3 +8,14 @@
 import Foundation
 
 // MARK: -
+
+enum TodayError: LocalizedError {
+    case failedReadingReminders
+
+    var errorDescription: String? {
+        switch self {
+        case .failedReadingReminders:
+            return NSLocalizedString("Failed to read reminders.", comment: "failed reading reminders error description")
+        }
+    }
+}
