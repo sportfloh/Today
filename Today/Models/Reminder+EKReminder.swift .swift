fd7ad045f85b379d6e4eb95c7a31bb57e4b1574d
Florian Bruder <florian.bruder@posteo.de>
2022-05-01 17:13:57 +0200

    Add initialiser to Reminder type to initialise it with an EKReminder

--- Today/Models/Reminder+EKReminder.swift .swift
+++ Today/Models/Reminder+EKReminder.swift .swift
@@ -9,3 +9,16 @@ import EventKit
 import Foundation
 
 // MARK: -
+
+extension Reminder {
+    init(with ekReminder: EKReminder) throws {
+        guard let dueDate = ekReminder.alarms?.first?.absoluteDate else {
+            throw TodayError.reminderHasNoDueDate
+        }
+        id = ekReminder.calendarItemExternalIdentifier
+        title = ekReminder.title
+        self.dueDate = dueDate
+        notes = ekReminder.notes
+        isComplete = ekReminder.isCompleted
+    }
+}
