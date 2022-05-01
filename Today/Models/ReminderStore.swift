5374f109d766c2b7719f88e73995865a7a54faef
Florian Bruder <florian.bruder@posteo.de>
2022-05-01 21:15:48 +0200

    Finish ReminderStore type

--- Today/Models/ReminderStore.swift
+++ Today/Models/ReminderStore.swift
@@ -9,3 +9,49 @@ import EventKit
 import Foundation
 
 // MARK: -
+
+class ReminderStore {
+    static let shared = ReminderStore()
+
+    private let ekStore = EKEventStore()
+
+    var isAvailable: Bool {
+        EKEventStore.authorizationStatus(for: .reminder) == .authorized
+    }
+
+    func requestAccess() async throws {
+        let status = EKEventStore.authorizationStatus(for: .reminder)
+        switch status {
+        case .notDetermined:
+            let accessGranted = try await ekStore.requestAccess(to: .reminder)
+            guard accessGranted else {
+                throw TodayError.accessDenied
+            }
+        case .restricted:
+            throw TodayError.accessRestricted
+        case .denied:
+            throw TodayError.accessDenied
+        case .authorized:
+            return
+        @unknown default:
+            throw TodayError.unknown
+        }
+    }
+
+    func readAll() async throws -> [Reminder] {
+        guard isAvailable else {
+            throw TodayError.accessDenied
+        }
+
+        let predicate = ekStore.predicateForReminders(in: nil)
+        let ekReminders = try await ekStore.fetchReminders(matching: predicate)
+        let reminders: [Reminder] = try ekReminders.compactMap { ekReminder in
+            do {
+                return try Reminder(with: ekReminder)
+            } catch TodayError.reminderHasNoDueDate {
+                return nil
+            }
+        }
+        return reminders
+    }
+}
