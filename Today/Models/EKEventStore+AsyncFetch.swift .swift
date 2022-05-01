342b62c8f3ed63bc73c7066e0a631c1166cbe81c
Florian Bruder <florian.bruder@posteo.de>
2022-05-01 17:01:35 +0200

    Implement async Reminder fetch function

--- Today/Models/EKEventStore+AsyncFetch.swift .swift
+++ Today/Models/EKEventStore+AsyncFetch.swift .swift
@@ -5,6 +5,21 @@
 //  Created by Florian Bruder on 01.05.22.
 //
 
+import EventKit
 import Foundation
 
 // MARK: -
+
+extension EKEventStore {
+    func fetchReminders(matching predicate: NSPredicate) async throws -> [EKReminder] {
+        try await withCheckedThrowingContinuation { continuation in
+            fetchReminders(matching: predicate) { reminders in
+                if let reminders = reminders {
+                    continuation.resume(returning: reminders)
+                } else {
+                    continuation.resume(throwing: TodayError.failedReadingReminders)
+                }
+            }
+        }
+    }
+}
