73e493c63b82cf3a7080ecaf9d63f990fb4a04ca
Florian Bruder <florian.bruder@posteo.de>
2022-05-02 14:51:38 +0200

    Fix SwiftLint line_length warnings

--- Today/Models/TodayError.swift
+++ Today/Models/TodayError.swift
@@ -20,17 +20,29 @@ enum TodayError: LocalizedError {
     var errorDescription: String? {
         switch self {
         case .accessDenied:
-            return NSLocalizedString("The app doesn't have permission to read reminders.", comment: "access denied error description")
+            return NSLocalizedString(
+                "The app doesn't have permission to read reminders.",
+                comment: "access denied error description")
         case .accessRestricted:
-            return NSLocalizedString("This device doesn't allow access to reminders.", comment: "access restricted error description")
+            return NSLocalizedString(
+                "This device doesn't allow access to reminders.",
+                comment: "access restricted error description")
         case .failedReadingCalendarItem:
-            return NSLocalizedString("Failed to read a calendar item.", comment: "failed reading calendar item error description")
+            return NSLocalizedString(
+                "Failed to read a calendar item.",
+                comment: "failed reading calendar item error description")
         case .failedReadingReminders:
-            return NSLocalizedString("Failed to read reminders.", comment: "failed reading reminders error description")
+            return NSLocalizedString(
+                "Failed to read reminders.",
+                comment: "failed reading reminders error description")
         case .reminderHasNoDueDate:
-            return NSLocalizedString("A reminder has no due date.", comment: "reminder has no due date error description")
+            return NSLocalizedString(
+                "A reminder has no due date.",
+                comment: "reminder has no due date error description")
         case .unknown:
-            return NSLocalizedString("An unknown error occurred.", comment: "unknown error description")
+            return NSLocalizedString(
+                "An unknown error occurred.",
+                comment: "unknown error description")
         }
     }
 }
